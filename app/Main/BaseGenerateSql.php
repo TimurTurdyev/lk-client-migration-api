f0761cd38a57a55c8730376c86c8a5e1d6223a81
timur <borodatimur@gmail.com>
2021-11-19 19:04:07 +0300

    Fix path #4

--- app/Main/BaseGenerateSql.php
+++ app/Main/BaseGenerateSql.php
@@ -63,7 +63,7 @@ class BaseGenerateSql
         $sql .= "SET @track_no = {$count};##$count" . PHP_EOL . PHP_EOL;
         $sql .= "INSERT INTO tree SET `path` = @path_to_tree, " . join(', ', $sql_create) . ";" . PHP_EOL . PHP_EOL;
 
-        if (self::$depth_static < $this->depth) {
+        if (self::$depth_static === 0 || self::$depth_static < $this->depth) {
             $sql .= "SET @last_tree_depth_{$this->depth} = LAST_INSERT_ID();" . PHP_EOL;
         }
 
